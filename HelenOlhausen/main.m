0174e26bf0e99912d7e14102b6cfc05fb854b7bc
Helen Olhausen <helen.olet@gmail.com>
2015-04-20 23:58:39 -0300

    Initial Commit

---
+++ HelenOlhausen/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HelenOlhausen
+//
+//  Created by Helen Olhausen on 4/20/15.
+//  Copyright (c) 2015 Helen Olhausen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
